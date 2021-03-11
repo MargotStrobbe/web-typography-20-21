5135b9ab499d1a3305ea456512eac19580edf2c1
MargotStrobbe <79916652+MargotStrobbe@users.noreply.github.com>
2021-03-11 17:47:00 +0100

    sounds

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,17 +1,16 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	10,
-	12,
-	13,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+	0,//sound 0 nummer1
+	9,//Sound 1 nummer 2
+	12,//sound 2 nummer 3
+	15,//sound 3 nummer 4
+	16.5,//sound 4 nummer 5
+	17,//sound 5 
+	24,//sound 6 nummer 6
+	25.8, //sound 7 nummer 7
+	31,//sound 8 nummer 8
+	32,//sound 9 nummer 9
+	34,//sound 10 nummer 10
+	86//sound 11 nummer 11
 ];
